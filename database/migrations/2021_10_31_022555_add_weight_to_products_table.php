df39e2705c8572f77067f582dc9cc373cafc7ef2
wiammuhammad@gmail.com <wiammuhammad@gmail.com>
2021-10-31 09:48:57 +0700

    dheling-store

--- database/migrations/2021_10_31_022555_add_weight_to_products_table.php
+++ database/migrations/2021_10_31_022555_add_weight_to_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddWeightFieldToProductsTable extends Migration
+class AddWeightToProductsTable extends Migration
 {
     /**
      * Run the migrations.
